70f8b9d953e6f0078a68174d243a93a267d76448
ro-nin <ema.cio1793@gmail.com>
2023-01-16 13:10:47 +0100

    cors fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://geo.ipify.org/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
